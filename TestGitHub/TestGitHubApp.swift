ea5f2b93a3b9795ef5a40d349354aeecd6ef0ae0
Juliano Vaz <julianoctvaz@gmail.com>
2024-08-26 10:22:37 -0300

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Juliano on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
